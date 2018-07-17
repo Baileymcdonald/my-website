05e249f95e0462ba890f7a9ed8880c90511585e6
Bailey McDonald <bailey@streammonkey.com>
2018-07-17 13:35:19 -0400

    navbar transition js

---
+++ _js/script.js
@@ -0,0 +1,20 @@
+// function myFunction() {
+//     var $nav = $(".navbar");
+//       $nav.toggleClass('clicked', $(this).onclick);
+// }
+// function myFunction1() {
+//     var $nav = $(".navbar");
+//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+// }
+$(document).ready(function() {
+    $(window).scroll(function () {
+      var $nav = $(".navbar");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+  $(document).ready(function() {
+    $(window).scroll(function () {
+      var $nav = $("a");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
